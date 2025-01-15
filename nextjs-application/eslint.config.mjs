423966c8c4599735c580d74859fbd95855c96e87
cotodevel <programmer.coto.1988@gmail.com>
2025-01-15 16:33:23 -0300

    switch codebase to: https://github.com/ixartz/Next-js-Boilerplate/commit/297b5d0c0f0dc2cd90bdfeccec43d208127ec799
    debugging working fine under vscode

--- nextjs-application/eslint.config.mjs
+++ nextjs-application/eslint.config.mjs
@@ -1,82 +1,58 @@
-import pluginJs from '@eslint/js';
+import antfu from '@antfu/eslint-config';
 import nextPlugin from '@next/eslint-plugin-next';
-
-import eslintConfigPrettier from 'eslint-config-prettier';
-import importPlugin from 'eslint-plugin-import';
-import pluginPromise from 'eslint-plugin-promise';
-import pluginReact from 'eslint-plugin-react';
+import jestDom from 'eslint-plugin-jest-dom';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import playwright from 'eslint-plugin-playwright';
 import tailwind from 'eslint-plugin-tailwindcss';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import testingLibrary from 'eslint-plugin-testing-library';
 
-export default [
-    {
-        files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}']
-    },
-    {
-        languageOptions: {
-            ecmaVersion: 'latest',
-            globals: { ...globals.browser, ...globals.node }
-        }
-    },
-    {
-        settings: {
-            react: {
-                version: 'detect'
-            }
-        }
-    },
-    pluginJs.configs.recommended, // ? https://github.com/eslint/eslint
-    importPlugin.flatConfigs.recommended, // ? https://github.com/import-js/eslint-plugin-import
-    ...tseslint.configs.recommended, // ? https://github.com/typescript-eslint/typescript-eslint
-    pluginPromise.configs['flat/recommended'], // ? https://github.com/eslint-community/eslint-plugin-promise
-    pluginReact.configs.flat.recommended, // ? https://github.com/jsx-eslint/eslint-plugin-react
-    pluginReact.configs.flat['jsx-runtime'], // ? https://github.com/jsx-eslint/eslint-plugin-react
-    eslintConfigPrettier, // ? https://github.com/prettier/eslint-config-prettier
-    ...tailwind.configs['flat/recommended'], // ? https://github.com/francoismassart/eslint-plugin-tailwindcss
-    {
-        rules: {
-            'no-unused-vars': 'off',
-            'react/react-in-jsx-scope': 'off',
-            'react-hooks/exhaustive-deps': 'off',
-            'react/display-name': 'off',
-            'react/prop-types': 'off',
-            '@typescript-eslint/no-unused-vars': 'off',
-            '@typescript-eslint/no-unused-expressions': 'off',
-            'tailwindcss/no-custom-classname': 'off',
-            'tailwindcss/migration-from-tailwind-2': 'off',
-            'import/no-unresolved': 'off',
-            'import/no-named-as-default': 'off',
-            // ! TO COMPILE SHADCN EXAMPLES, PLEASE REMOVE AS NEEDED
-            '@typescript-eslint/no-explicit-any': 'off',
-            '@typescript-eslint/no-empty-object-type': 'off',
-            '@typescript-eslint/ban-ts-comment': 'off',
-            'react/no-unescaped-entities': 'off',
-            'react/no-unknown-property': 'off',
-            'tailwindcss/no-unnecessary-arbitrary-value': 'off',
-            'tailwindcss/classnames-order': 'off',
-            'import/named': 'off',
-            'import/no-named-as-default-member': 'off'
-        }
-    },
-    // ! ===================== DISCLAIMER =====================
-    // ! There is no official solution available for new ESLint 9 flat config structure for NextJS
-    // ! The solution is taken from the community and may not be the best practice, use it at your own risk
-    // ? Ref: https://github.com/vercel/next.js/discussions/49337?sort=top#discussioncomment-5998603
-    // ! ======================================================
-    {
-        plugins: {
-            '@next/next': nextPlugin
-        },
-        rules: {
-            ...nextPlugin.configs.recommended.rules,
-            ...nextPlugin.configs['core-web-vitals'].rules,
-            '@next/next/no-img-element': 'off',
-            // ! TO COMPILE SHADCN EXAMPLES, PLEASE REMOVE AS NEEDED
-            '@next/next/no-html-link-for-pages': 'off'
-        }
-    },
-    {
-        ignores: ['.next/*']
-    }
-];
+export default antfu({
+  react: true,
+  typescript: true,
+
+  lessOpinionated: true,
+  isInEditor: false,
+
+  stylistic: {
+    semi: true,
+  },
+
+  formatters: {
+    css: true,
+  },
+
+  ignores: [
+    'migrations/**/*',
+    'next-env.d.ts',
+  ],
+}, ...tailwind.configs['flat/recommended'], jsxA11y.flatConfigs.recommended, {
+  plugins: {
+    '@next/next': nextPlugin,
+  },
+  rules: {
+    ...nextPlugin.configs.recommended.rules,
+    ...nextPlugin.configs['core-web-vitals'].rules,
+  },
+}, {
+  files: [
+    '**/*.test.ts?(x)',
+  ],
+  ...testingLibrary.configs['flat/react'],
+  ...jestDom.configs['flat/recommended'],
+}, {
+  files: [
+    '**/*.spec.ts',
+    '**/*.e2e.ts',
+  ],
+  ...playwright.configs['flat/recommended'],
+}, {
+  rules: {
+    'antfu/no-top-level-await': 'off', // Allow top-level await
+    'style/brace-style': ['error', '1tbs'], // Use the default brace style
+    'ts/consistent-type-definitions': ['error', 'type'], // Use `type` instead of `interface`
+    'react/prefer-destructuring-assignment': 'off', // Vscode doesn't support automatically destructuring, it's a pain to add a new variable
+    'node/prefer-global/process': 'off', // Allow using `process.env`
+    'test/padding-around-all': 'error', // Add padding in test files
+    'test/prefer-lowercase-title': 'off', // Allow using uppercase titles in test titles
+  },
+});
